5c10586f1cb5b9fd911e78b35222ff3688f63b3c
BuzzingBeaver <f20170728@pilani.bits-pilani.ac.in>
2019-10-13 12:45:34 +0530

    Q2 triplicate left, rest done.

--- q2.pl
+++ q2.pl
@@ -1,3 +1,8 @@
 sublist([],Y).
 sublist([A|X],[A|Y]):-sublist(X,Y).
 sublist(X,[A|Y]):-sublist(X,Y).
+remove_every_other([],[]).
+remove_every_other([X],[X]).
+remove_every_other([A,_|X],[A|Y]):-remove_every_other(X,Y).
+remove_nth(1,[A|X],X).
+remove_nth(N,[A|X],[A|Y]):-N1 is N-1,remove_nth(N1,X,Y).
